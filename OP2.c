bbf1cc337b9debf57928f8c80778cff27ac59bbc
htulahpai-spec <htu.lahpai@auw.edu.bd>
2026-01-30 21:31:31 +0600

    Add files via upload

---
+++ OP2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    float c, f;
+    printf("Enter temperature in Celsius: ");
+    scanf("%f", &c);
+
+    f = (c * 9 / 5) + 32;
+    printf("Trmperature in Fahrenheit = %.2f\n", f);
+    return 0;
+
+}
+
+
