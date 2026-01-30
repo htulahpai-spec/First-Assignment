456e2d1a6a8233633d5e9378beeaf529c4901973
htulahpai-spec <htu.lahpai@auw.edu.bd>
2026-01-30 21:32:41 +0600

    Add files via upload

---
+++ OP4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int x = 5;
+
+    printf("Initial value: %d\n", x);
+    x++;
+    printf("After increment: %d\n", x);
+    x--;
+    printf("After decrement: %d\n", x);
+
+    return 0;
+
+}
+
+
+
+
